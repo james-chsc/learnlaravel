4b5206e477858c17c86967972fc4a72e9c7982e7
james <tw.jameslee@gmail.com>
2015-03-03 22:48:33 +0800

    練習2015/3/3

--- app/controllers/admin/AuthController.php
+++ app/controllers/admin/AuthController.php
@@ -2,94 +2,137 @@
 
 namespace App\Controllers\Admin;
 
-use Illuminate\Support\Facades\Auth;
-use Symfony\Component\DomCrawler\Form;
-use Symfony\Component\Console\Input\Input;
-use Illuminate\Support\Facades\Redirect;
-use Cartalyst\Sentry;
-use Illuminate\Support\Facades\View;
+use \Auth, \BaseController, \Form, \Input, \Redirect, \Sentry, \View;
 
-class AuthController extends \BaseController {
+class AuthController extends BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /admin/auth
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
+    /**
+     * 顯示登入頁面
+     * @return View
+     */
+    public function getLogin()
+    {
+        return View::make('admin.auth.login');
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /admin/auth/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    /**
+     * 登入驗證
+     * @return Redirect
+     */
+    public function postLogin()
+    {
+        $credentials = [
+            'email' => Input::get('email'),
+            'password' => Input::get('password')
+        ];
 
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /admin/auth
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+        try
+        {
+            $user = Sentry::authenticate($credentials, false);
 
-	/**
-	 * Display the specified resource.
-	 * GET /admin/auth/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+            if ($user)
+            {
+                return Redirect::route('admin.pages.index');
+            }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /admin/auth/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+        } catch (\Exception $e)
+        {
+            return Redirect::route('admin.login')
+                           ->withErrors(['login' => $e->getMessage()]);
+        }
 
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /admin/auth/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /admin/auth/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * 登出
+     * @return Redirect
+     */
+    public function getLogout()
+    {
+        Sentry::logout();
+
+        return Redirect::route('admin.login');
+    }
+
+    /**
+     * Display a listing of the resource.
+     * GET /admin/auth
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * GET /admin/auth/create
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * POST /admin/auth
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     * GET /admin/auth/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * GET /admin/auth/{id}/edit
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * PUT /admin/auth/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * DELETE /admin/auth/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 }
--- app/routes.php
+++ app/routes.php
@@ -4,3 +4,28 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('admin/logout', [
+    'as'    => 'admin.logout',
+    'uses'  => 'App\Controllers\Admin\AuthController@getLogout'
+]);
+
+Route::get('admin/login', [
+    'as'    =>  'admin.login',
+    'uses'  =>  'App\Controllers\Admin\AuthController@getLogin'
+]);
+
+Route::post('admin/login', [
+    'as'    =>  'admin.login.post',
+    'uses'  =>  'App\Controllers\Admin\AuthController@postLogin'
+]);
+
+Route::group([
+    'prefix'    =>  'admin',
+    'before'    =>  'auth.admin'    // filter in app/filter.php
+], function()
+{
+    Route::any('/', 'App\Controllers\Admin\PageController@index');
+    Route::resource('articles', 'App\Controllers\Admin\ArticlesController');
+    Route::resource('pages', 'App\Controllers\Admin\PagesController');
+});
